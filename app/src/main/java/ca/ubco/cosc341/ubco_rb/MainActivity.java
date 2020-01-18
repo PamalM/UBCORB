d46f00280f12ff7277f0e111334ad339de61846f
Pamal Mangat <Pamal13Mangat@Gmail.com>
2020-01-18 14:53:48 -0800

    First Commit

---
+++ app/src/main/java/ca/ubco/cosc341/ubco_rb/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.ubco.cosc341.ubco_rb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
