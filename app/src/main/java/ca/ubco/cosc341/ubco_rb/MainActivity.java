97ec8e48c055b03d1be3514e42cf873b30464c0d
Pamal Mangat <pamal13mangat@gmail.com>
2020-01-18 19:52:16 -0800

    - Added the Date, Time, Building, Room# textViews to home screen.
    - Added Search/Submit button to home screen.
    
    - Added blue background ImageView for easier visuality for textViews typing.
    
    - Re-Positioned constraints for textViews, button, title.jpg imgView on home screen.

--- app/src/main/java/ca/ubco/cosc341/ubco_rb/MainActivity.java
+++ app/src/main/java/ca/ubco/cosc341/ubco_rb/MainActivity.java
@@ -3,6 +3,7 @@ package ca.ubco.cosc341.ubco_rb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello World.");
+
     }
+
 }
