c76f1e4fae2b9f9486f607fa7833bc96e4b47905
Pamal Mangat <Pamal13Mangat@Gmail.com>
2020-01-19 08:21:11 -0800

    Search Screen Backup commit.

--- app/src/main/java/ca/ubco/cosc341/ubco_rb/resultPage.java
+++ app/src/main/java/ca/ubco/cosc341/ubco_rb/resultPage.java
@@ -2,13 +2,43 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class resultPage extends AppCompatActivity {
+ public class resultPage extends AppCompatActivity {
+
+     Button returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_page);
+
+        returnButton = (Button) findViewById(R.id.returnButton);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+
+            //Method will execute when user clicks search button.
+            //Grab current values in all text boxes, and save into variables.
+            @Override
+            public void onClick(View v) {
+
+                //Method redirects user back to home/search screen.
+                returnHome();
+
+            }
+        });
+
+
     }
+
+     public void returnHome() {
+
+         Intent intent = new Intent(this, MainActivity.class);
+         startActivity(intent);
+
+     }
+
 }
