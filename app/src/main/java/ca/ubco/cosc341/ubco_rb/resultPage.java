d3e90af7ae65c03fe623ab1a3a21f66b48d794bf
Pamal Mangat <pamal13mangat@gmail.com>
2020-01-19 08:51:24 -0800

    Search Screen Backup commit.

--- app/src/main/java/ca/ubco/cosc341/ubco_rb/resultPage.java
+++ app/src/main/java/ca/ubco/cosc341/ubco_rb/resultPage.java
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
  public class resultPage extends AppCompatActivity {
 
      Button returnButton;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_page);
+
+        //Setup listview to accommodate search results.
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        
+
 
         returnButton = (Button) findViewById(R.id.returnButton);
 
