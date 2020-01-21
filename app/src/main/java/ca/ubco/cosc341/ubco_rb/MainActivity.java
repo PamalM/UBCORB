2fb4205352677f09874cb406651ae1edecfce430
Pamal Mangat <Pamal13Mangat@Gmail.com>
2020-01-21 08:56:51 -0800

    Commit

--- app/src/main/java/ca/ubco/cosc341/ubco_rb/MainActivity.java
+++ app/src/main/java/ca/ubco/cosc341/ubco_rb/MainActivity.java
@@ -2,17 +2,14 @@ package ca.ubco.cosc341.ubco_rb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
-import android.content.Context;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     //Widgets.
-    EditText dateEntry, hhEntry, mmEntry, timeOfDayEntry, buildingEntry, roomEntry;
-    Button sumbitButton;
+    Button searchButton;
 
     //Text-Entry fields that will be passed to conduct search query.
     String room, hours, minutes, building, am_Identifier, date;
@@ -22,35 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Find textViews with ids.
-        dateEntry = (EditText) findViewById(R.id.dateEntry);
-        hhEntry = (EditText) findViewById(R.id.hhEntry);
-        mmEntry = (EditText) findViewById(R.id.mmEntry);
-        timeOfDayEntry = (EditText) findViewById(R.id.timeOfDayEntry);
-        buildingEntry = (EditText) findViewById(R.id.buildingEntry);
-        roomEntry = (EditText) findViewById(R.id.roomEntry);
-
         //Find button with id.
-        sumbitButton = (Button) findViewById(R.id.submitButton);
+        searchButton = (Button) findViewById(R.id.searchButton);
 
         //Set search button onClick event handler.
-        sumbitButton.setOnClickListener(new View.OnClickListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
 
             //Method will execute when user clicks search button.
             //Grab current values in all text boxes, and save into variables.
             @Override
             public void onClick(View v) {
-                //Fetch and save the entered values into variables to be passed on later.
-                room = roomEntry.getText().toString();
-                hours = hhEntry.getText().toString();
-                minutes = mmEntry.getText().toString();
-                am_Identifier = timeOfDayEntry.getText().toString();
-                building = buildingEntry.getText().toString();
-                room = roomEntry.getText().toString();
-                date = dateEntry.getText().toString();
 
-                //Pass saved values onto method to be processed/printed to terminal and sent over to the search query.
-                passForQuerying(date, hours, minutes, am_Identifier, building, room);
+                System.out.println("[Search Button Clicked]");
+
             }
         });
     }
